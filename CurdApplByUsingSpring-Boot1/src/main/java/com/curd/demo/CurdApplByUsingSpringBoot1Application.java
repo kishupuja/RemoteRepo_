181e00ce61560c2f5ca30b3d82f261d668c15178
kishupuja <kishupuja89@gmail.com>
2019-06-23 19:43:00 -0700

    I have Added some piece of code now

---
+++ CurdApplByUsingSpring-Boot1/src/main/java/com/curd/demo/CurdApplByUsingSpringBoot1Application.java
@@ -0,0 +1,13 @@
+package com.curd.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdApplByUsingSpringBoot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdApplByUsingSpringBoot1Application.class, args);
+	}
+
+}
---
+++ CurdApplByUsingSpring-Boot1/src/test/java/com/curd/demo/CurdApplByUsingSpringBoot1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.curd.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CurdApplByUsingSpringBoot1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
